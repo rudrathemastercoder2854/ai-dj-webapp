2d23a43abdddc84bce1b013163dd32416dc36a60
Coder Rudra <92848174+rudrathemastercoder2854@users.noreply.github.com>
2024-01-04 15:56:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 song = "";
 scoreLeftWrist = 0;
+scoreRightWrist=0;
 rightWristX = 0;
 rightWristY = 0;
 leftWristX = 0;
@@ -29,7 +30,36 @@ function modelLoaded() {
         document.getElementById("volume").innerHTML = "Volume = " + volume;
         song.setVolume(volume);		
     }
+    if(scoreRightWrist>0.2){
+    circle(rightWristX,rightWristY,20);
+    if(rightWristY>0 && rightWristY<=100){
+      document.getElementById("speed").innerHTML="speed:0.5x";
+      song.rate(0.5);
+
+    }
+    else if(rightWristY>100 && rightWristY<=200){
+      document.getElementById("speed").innerHTML="speed:0.75x";
+      song.rate(0.75);
+
+    }
+    else if(rightWristY>200 && rightWristY<=300){
+      document.getElementById("speed").innerHTML="speed:Normal";
+      song.rate(1);
+
+    }
+    else if(rightWristY>300 && rightWristY<=400){
+      document.getElementById("speed").innerHTML="speed:1.5x";
+      song.rate(1.5);
+
+    }
+    else if(rightWristY>400 && rightWristY<=530){
+      document.getElementById("speed").innerHTML="speed:2x";
+      song.rate(2);
+
+    }
 }
+  }
+
 
 
 function preload() {
@@ -47,6 +77,8 @@ function gotPoses(results)
 	console.log(results);
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
 	console.log("scoreLeftWrist = " + scoreLeftWrist);
+  scoreRightWrist =  results[0].pose.keypoints[10].score;
+	console.log("scoreRightWrist = " + scoreRightWrist +  "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
